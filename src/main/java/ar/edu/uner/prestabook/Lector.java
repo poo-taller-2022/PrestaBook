0918585698c0f725ba1c3b38cf9cdadd53dc86c1
Geronimo Giovenale <gero.giovenale@gmail.com>
2022-09-13 18:26:56 -0300

    fixed diagram and added annotation on Lector

--- src/main/java/ar/edu/uner/prestabook/Lector.java
+++ src/main/java/ar/edu/uner/prestabook/Lector.java
@@ -3,6 +3,10 @@ package ar.edu.uner.prestabook;
 import java.util.Collections;
 import java.util.List;
 
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = true)
 public class Lector extends Persona {
 
 	public void reservarObra(Obra obra) {
