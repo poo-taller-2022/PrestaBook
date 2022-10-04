d30cebe07c6adec11f195009576bdf3c0a9f4e1d
Taiel Altamirano <taiel.alt97@gmail.com>
2022-10-04 17:14:23 -0300

    agregación de tipoObraDao y creación de la tabla tipos_obra

---
+++ src/main/java/ar/edu/uner/prestabook/persistence/ITipoObraDAO.java
@@ -0,0 +1,8 @@
+package ar.edu.uner.prestabook.persistence;
+
+import ar.edu.uner.prestabook.common.GenericDAO;
+import ar.edu.uner.prestabook.model.TipoObra;
+
+public interface  ITipoObraDAO extends GenericDAO<TipoObra> {
+
+}
