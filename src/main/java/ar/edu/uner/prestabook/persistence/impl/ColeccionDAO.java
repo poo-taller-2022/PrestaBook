fb5bed1ffa81de020b37a56f7c4f671754a60486
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-18 21:44:41 -0300

    javadocs added to ColeccionDAO

--- src/main/java/ar/edu/uner/prestabook/persistence/impl/ColeccionDAO.java
+++ src/main/java/ar/edu/uner/prestabook/persistence/impl/ColeccionDAO.java
@@ -29,17 +29,35 @@ public class ColeccionDAO implements IColeccionDAO {
         return instance;
     }
 
+    
+    /**
+     * 
+     * get all entities in table colecciones from database
+     * @return list of objects Coleccion
+     */
     @Override
     public List<Coleccion> findAll() {
         return HibernateConnection.getCurrentSession().createQuery("from Coleccion", Coleccion.class).list();
 
     }
 
+    
+    /**
+     * get an entity from table <> where id match with param 
+     * @return Coleccion object
+     * @param id of Coleccion
+     */
     @Override
     public Coleccion findById(Object id) {
         return HibernateConnection.getCurrentSession().get(Coleccion.class, (String) id);
     }
 
+    
+    /**
+     * inserts an entity Coleccion in database
+     * @return Coleccion The most recently object inserted
+     * @param Coleccion object
+     */
     @Override
     public Coleccion insert(Coleccion coleccion) {
         Transaction tx = HibernateConnection.getCurrentSession().beginTransaction();
@@ -49,17 +67,18 @@ public class ColeccionDAO implements IColeccionDAO {
         return coleccion;
     }
 
+    
+    /**
+     * update table colecciones in database with new object Coleccion
+     * @return Coleccion the object updated
+     * @param Coleccion object to update
+     */
     @Override
     public Coleccion update(Coleccion coleccion) {
         Transaction tx = HibernateConnection.getCurrentSession().beginTransaction();
         HibernateConnection.getCurrentSession().update(coleccion);
         tx.commit();
         return coleccion;
-    }
-
-    @Override
-    public Integer delete(Coleccion t) {
-        return null;
     }
 
 }
