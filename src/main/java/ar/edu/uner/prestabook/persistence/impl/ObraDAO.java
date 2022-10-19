abe92df76f9f4210e58870e47a13bdc91fae95e0
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-18 22:23:29 -0300

    javadocs added to ObraDAO

--- src/main/java/ar/edu/uner/prestabook/persistence/impl/ObraDAO.java
+++ src/main/java/ar/edu/uner/prestabook/persistence/impl/ObraDAO.java
@@ -29,17 +29,35 @@ public class ObraDAO implements IObraDAO {
         return instance;
     }
 
+    
+    /**
+     * 
+     * get all entities in table obras from database
+     * @return list of objects Obra
+     */
     @Override
     public List<Obra> findAll() {
         return HibernateConnection.getCurrentSession().createQuery("from Obra", Obra.class).list();
     }
 
+    
+    /**
+     * get an entity from table Obras where id match with param 
+     * @return Obra object
+     * @param id of Obra
+     */
     @Override
     public Obra findById(Object id) {
         return HibernateConnection.getCurrentSession().get(Obra.class, (String) id);
 
     }
 
+    
+    /**
+     * inserts an entity Obra in database
+     * @return Obra The most recently object inserted
+     * @param Obra object
+     */
     @Override
     public Obra insert(Obra obra) {
         Transaction tx = HibernateConnection.getCurrentSession().beginTransaction();
@@ -49,6 +67,12 @@ public class ObraDAO implements IObraDAO {
         return obra;
     }
 
+    
+    /**
+     * update table obras in database with new object Obra
+     * @return Obra The object updated
+     * @param Obra object to update
+     */
     @Override
     public Obra update(Obra obra) {
         Transaction tx = HibernateConnection.getCurrentSession().beginTransaction();
@@ -57,11 +81,12 @@ public class ObraDAO implements IObraDAO {
         return obra;
     }
 
-    @Override
-    public Integer delete(Obra t) {
-        return null;
-    }
-
+    
+    /**
+     * get all entities from table obras where id match with param 
+     * @return list of objects Obra
+     * @param id of Obra
+     */
     public List<Obra> filtrarPorTema(Integer id) {
         return HibernateConnection.getCurrentSession()
                 .createQuery(String.format("FROM Obra obra WHERE obra.area = %s", id), Obra.class).list();
